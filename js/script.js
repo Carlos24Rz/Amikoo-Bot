324abcedb585a24b503b267f8ea3acf5fa7e4329
JAngelGC <a01552274@tec.mx>
2022-03-13 00:42:27 -0600

    Add buttons to add chatbot msgs

--- js/script.js
+++ js/script.js
@@ -1,68 +1,202 @@
+const chatbotBox = document.querySelector(".chatbot-box");
 const chatbotChat = document.querySelector(".chatbot-chat");
-const btnAdd = document.querySelector(".button-bot");
-
-const html1 = `<div class="chatbot-msg">
-<img class="logo--chat" src="./img/Logo-header.svg" alt="" />
-
-<div class="chatbot-msg-content">
-  <p class="chatbot-text">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
-    libero, sit eum voluptates natus voluptatem dolorum. Beatae sit.
-  </p>
-</div>
-</div>`;
-
-const html2 = `<div class="chatbot-msg chatbot-msg-user">
-<img class="logo--chat" src="./img/Logo-header.svg" alt="" />
-<div class="chatbot-msg-content">
-  <p class="chatbot-text">Lorem ipsum dolor sit amet.</p>
-</div>
-</div>`;
-
-let counter = 1;
-let check = false;
-
-btnAdd.addEventListener("click", function () {
-  if (check) {
-    if (counter % 2 == 0) {
-      chatbotChat.insertAdjacentHTML("beforeend", html2);
-      counter++;
-      chatbotChat.scrollTo(0, chatbotChat.scrollHeight);
-    } else {
-      chatbotChat.insertAdjacentHTML("beforeend", html1);
-      counter++;
-      chatbotChat.scrollTo(0, chatbotChat.scrollHeight);
-    }
-  }
-});
-
-const chatbotContainer = document.querySelector(".chatbot-box");
 const chatbotFace = document.querySelector(".chatbot-face");
-
-chatbotFace.addEventListener("click", function () {
-  chatbotContainer.classList.toggle("hidden");
-  chatbotFace.classList.toggle("hidden");
-  check = true;
-});
 
 const chatbotExit = document.querySelector(".chatbot-close-button");
 
-chatbotExit.addEventListener("click", function () {
-  chatbotContainer.classList.toggle("hidden");
-  chatbotFace.classList.toggle("hidden");
-  check = false;
+// Function that put down the scroll bar
+const updateScrollBar = function () {
+  // chatbotChat.scrollIntoView({
+  //   behavior: "smooth",
+  //   block: "end",
+  //   inline: "end",
+  // });
+
+  chatbotChat.scrollTo(0, chatbotChat.scrollHeight);
+};
+
+// Function to hide and show the chatbot box
+const handleShowChatbot = function () {
+  chatbotFace.addEventListener("click", function () {
+    chatbotBox.classList.toggle("hidden");
+    chatbotFace.classList.toggle("hidden");
+  });
+
+  chatbotExit.addEventListener("click", function () {
+    chatbotBox.classList.toggle("hidden");
+    chatbotFace.classList.toggle("hidden");
+  });
+
+  updateScrollBar();
+  // chatbotChat.scrollTo(0, chatbotChat.scrollHeight);
+};
+
+handleShowChatbot();
+
+// Menu to add texts box in the chatbot
+const btnChatbotText = document.querySelector(".btn-chatbot-text-face");
+const btnChatbotTextNoface = document.querySelector(
+  ".btn-chatbot-text-no-face"
+);
+const btnChatbotReview = document.querySelector(".btn-chatbot-review");
+const btnChatbotLoading = document.querySelector(".btn-chatbot-loading");
+const btnChatbotOptions = document.querySelector(".btn-chatbot-options");
+const btnChatbotUser = document.querySelector(".btn-chatbot-user");
+
+const htmlChatbotText = (text) => {
+  return `
+  <div class="chatbot-msg">
+    <img class="logo--chat" src="./img/Logo-header.svg" alt="" />
+
+    <div class="chatbot-msg-content">
+      <p class="chatbot-text">
+        ${text}
+      </p>
+    </div>
+  </div>
+`;
+};
+
+const htmlChatbotTextNoface = (text) => {
+  return `
+  <div class="chatbot-msg chatbot-msg-no-face">
+    <img class="logo--chat" src="./img/Logo-header.svg" alt="" />
+
+    <div class="chatbot-msg-content">
+      <p class="chatbot-text">
+        ${text}
+      </p>
+    </div>
+  </div>
+`;
+};
+
+const htmlChatbotReview = () => {
+  return `
+  <div class="chatbot-msg">
+    <img class="logo--chat" src="./img/Logo-header.svg" alt="" />
+
+    <div class="container-stars chatbot-msg-content">
+      <div class="star-widget">
+        <input type="radio" name="rate" id="rate-5" />
+        <label for="rate-5" class="fa-solid fa-star"></label>
+        <input type="radio" name="rate" id="rate-4" />
+        <label for="rate-4" class="fa-solid fa-star"></label>
+        <input type="radio" name="rate" id="rate-3" />
+        <label for="rate-3" class="fa-solid fa-star"></label>
+        <input type="radio" name="rate" id="rate-2" />
+        <label for="rate-2" class="fa-solid fa-star"></label>
+        <input type="radio" name="rate" id="rate-1" />
+        <label for="rate-1" class="fa-solid fa-star"></label>
+        <form action="#">
+          <header class="title-star">&nbsp;</header>
+          <div class="submit-box">
+            <button type="submit">Post</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>`;
+};
+
+const htmlChatbotLoading = () => {
+  return `
+  <div class="chatbot-msg">
+    <img class="logo--chat" src="./img/Logo-header.svg" alt="" />
+
+    <div class="chatbot-msg-content loader">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  </div>
+`;
+};
+
+const htmlChatbotOptions = (text, ...options) => {
+  htmlOptions = options
+    .map((option) => `<p class="chatbot-option">${option}</p>`)
+    .join("");
+
+  console.log(htmlOptions);
+
+  return `
+          <div class="chatbot-msg">
+            <img class="logo--chat" src="./img/Logo-header.svg" alt="" />
+            <div class="chatbot-msg-content">
+              <div class="chatbot-options">
+                <p class="chatbot-options-text">
+                  ${text}
+                </p>
+                ${htmlOptions}
+              </div>
+            </div>
+          </div>
+  `;
+};
+
+const htmlUserInput = (text) => {
+  return `
+  <div class="chatbot-msg chatbot-msg-user">
+    <img class="logo--chat" src="./img/Logo-header.svg" alt="" />
+    <div class="chatbot-msg-content">
+      <p class="chatbot-text">${text}</p>
+    </div>
+  </div>`;
+};
+
+const someText =
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum libero, sit eum voluptates natus voluptatem dolorum. Beatae sit.";
+
+btnChatbotText.addEventListener("click", function () {
+  chatbotChat.insertAdjacentHTML("beforeend", htmlChatbotText(someText));
+
+  updateScrollBar();
 });
 
-chatbotChat.scrollIntoView({
-  behavior: "smooth",
-  block: "end",
-  inline: "end",
+btnChatbotTextNoface.addEventListener("click", function () {
+  chatbotChat.insertAdjacentHTML("beforeend", htmlChatbotTextNoface(someText));
+  updateScrollBar();
 });
 
-// ANIMATION CHATBOT
+btnChatbotReview.addEventListener("click", function () {
+  chatbotChat.insertAdjacentHTML("beforeend", htmlChatbotReview());
+  updateScrollBar();
+});
 
-// chatbotFace.addEventListener("mouseover", function () {
-//   const rightEye = document.querySelector("#Eye-right");
-//   console.log(rightEye);
-//   // rightEye.x = "1.2rem";
-// });
+btnChatbotLoading.addEventListener("click", function () {
+  chatbotChat.insertAdjacentHTML("beforeend", htmlChatbotLoading());
+  updateScrollBar();
+});
+
+btnChatbotOptions.addEventListener("click", function () {
+  chatbotChat.insertAdjacentHTML(
+    "beforeend",
+    htmlChatbotOptions(someText, 1, 2, 3, 4)
+  );
+  updateScrollBar();
+});
+
+btnChatbotUser.addEventListener("click", function () {
+  chatbotChat.insertAdjacentHTML("beforeend", htmlUserInput(someText));
+  updateScrollBar();
+});
+
+// SEARCH BAR
+const searchBarEl = document.querySelector(".search-bar");
+
+searchBarEl.addEventListener("keyup", function (e) {
+  e.preventDefault();
+  if (e.key === "Enter" || e.keyCode === 13) {
+    const html3 = `<div class="chatbot-msg chatbot-msg-user">
+                    <img class="logo--chat" src="./img/Logo-header.svg" alt="" />
+                    <div class="chatbot-msg-content">
+                      <p class="chatbot-text">${searchBarEl.value}</p>
+                    </div>
+                  </div>`;
+    chatbotChat.insertAdjacentHTML("beforeend", html3);
+    searchBarEl.value = "";
+    // chatbotChat.scrollTo(0, chatbotChat.scrollHeight);
+    updateScrollBar();
+  }
+});
