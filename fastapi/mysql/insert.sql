bf86103403f19e8c8ded4145a797f7d9141dd195
JAngelGC <a01552274@tec.mx>
2022-04-24 11:44:43 -0500

    Added notas nuevas en NOTAS.txt

--- fastapi/mysql/insert.sql
+++ fastapi/mysql/insert.sql
@@ -1,3 +1,36 @@
+INSERT INTO pregunta (padre_id, nombre, emoji, texto, visitas, is_final) VALUES
+  (null,	"Inicio",	😎,	"texto inicio",	0,	false),
+  (1,	"Nosotros",	😎,	"texto nosotros",	0,	false),
+  (1,	"Lineas de negocio",	🛒,	"texto lineas de negocio",	0,	false),
+  (1,	"Proyectos y Reconocimientos",	😎,	"texto proyectos y reconocimientos",	0,	false),
+  (1,	"Aplicaciones",	👨‍🔧,	"texto aplicaciones",	0,	false),
+  (1,	"Oportunidades",	🤩,	"texto oportunidades",	0,	false),
+  (1,	"Contacto",	📞,	"mostrarFormulario()",	0,	true),
+  (2,	"¿Quienes somos?",	❓,	"texto quienes somos",	0,	false),
+  (2,	"Misión",	🎯,	"texto mision",	0,	true),
+  (2,	"Visión",	👀,	"texto vision",	0,	true),
+  (2,	"Valores",	❤	,"texto valores",	0,	true),
+  (2,	"Colaboradores",	🤝,	"texto colaboradores",	0,	false),
+  (8,	"Nombre de la empresa",	🏭,	"texto nombre empresa",	0,	true),
+  (8,	"¿Qué significa Naatik?",	🤔,	"texto que significa",	0,	true),
+  (8,	"Historia",	🧐,	"texto historia",	0,	true),
+  (8,	"Claves de registro nacionales",	📝,	"texto claves",	0,	true),
+  (12,	"Socios principales",	📝,	"texto socios principales",	0,	true),
+  (12,	"Instituciones",	📝,	"texto instituciones",	0,	true),
+  (12,	"Empresas hermanas",	📝,	"texto empresas hermanas",	0,	true),
+  (3,	"Desarrollo de sistemas informáticos",	📝,	"Linea de negocio 1",	0,	true),
+  (3,	"Vinculación Academia-Industria",	📝,	"Linea de negocio 2",	0,	true),
+  (3,	"Atención a convocatorias para desarrollo de proyectos",	📝,	"Linea de negocio 3",	0,	true),
+  (6,	"Estancias para tesis o servicio social",	📝,	"texto estancias",	0,	false),
+  (6,	"¿Cómo podemos contactarlos?",	📝,	"texto como contactar",	0,	true),
+  (23,	"Requisitos",	📝,	"texto requisitos",	0,	true),
+  (23,	"Beneficios",	📝,	"texto beneficios",	0,	true),
+  (23,	"Documentacion requerida",	📝,	"texto documentacion",	0,	true),
+  (23,	"Actividades a realizar",	📝,	"texto actividades",	0,	true),
+  (23,	"Cómo aplicar",	📝,	"texto como aplicar",	0,	true);
+
+
+
 
 INSERT INTO categoria (nombre, texto, visitas, is_final) VALUES
   ("Inicio",	"texto inicio",	0,	false),
