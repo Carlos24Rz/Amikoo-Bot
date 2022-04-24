3a729c06db02e4c16424b78a21e8d279e1e8fb5d
Diaz <carlosd2mil@gmail.com>
2022-04-23 21:58:51 -0500

    Testing files

---
+++ fastapi/fetch/js/super-simple.js
@@ -0,0 +1,20 @@
+
+console.log("works")
+
+var simple_chart_config = {
+	chart: {
+		container: "#OrganiseChart-simple"
+	},
+
+	nodeStructure: {
+		text: { name: "Inicio 🏄" },
+		children: [
+			{
+				text: { name: "Nosotros" }
+			},
+			{
+				text: { name: "Lineas" }
+			}
+		]
+	}
+};
