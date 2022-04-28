2d4d5970630b829958385152871e8ddbcf81829b
Diaz <carlosd2mil@gmail.com>
2022-04-28 16:45:08 -0500

    select testing

---
+++ fastapi/mysql/select.sql
@@ -0,0 +1,53 @@
+
+-- Recursive select of all childs of $id
+WITH RECURSIVE cte (id, padre_id, nombre, emoji) AS (
+  SELECT     id,
+             padre_id,
+             nombre,
+             emoji
+  FROM       pregunta
+  WHERE      id = 2
+  UNION ALL
+  SELECT     id,
+             padre_id,
+             nombre,
+             emoji
+  FROM       pregunta
+  WHERE      padre_id = 2
+  UNION ALL
+  SELECT     p.id,
+             p.padre_id,
+             p.nombre,
+             p.emoji
+  FROM       pregunta p
+  INNER JOIN cte
+          ON p.padre_id = cte.id
+)
+SELECT * FROM cte;
+
+
+-- All rows with their parents row name
+select a.id, a.nombre, a.emoji, b.nombre as parent
+from pregunta a
+left join pregunta b on a.padre_id = b.id
+where b.nombre = "Inicio"
+
+-- All rows with their parents row name
+select pregunta.id, pregunta.nombre, pregunta.emoji, b.nombre as parent
+from pregunta
+left join pregunta b on pregunta.padre_id = b.id
+where b.nombre = "Inicio"
+
+-- Get Parent of a pregunta
+select pregunta.id, pregunta.nombre, pregunta.emoji, b.nombre as parent
+from pregunta
+left join pregunta b on pregunta.padre_id = b.id
+where pregunta.nombre = "Nosotros"
+
+
+select id, nombre, emoji
+from pregunta
+where id = (select padre_id
+            from pregunta
+            where pregunta.nombre = "Nosotros"
+            )
