6b71e45ca34562036fdd12d490362aa33fdfbc6b
JAngelGC <a01552274@tec.mx>
2022-05-01 19:26:09 -0500

    Added algunas animaciones

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
+const chatbotLogoFace = `
+<svg class="logo--chat" width="43" height="32" viewBox="0 0 43 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g id="Logo-chat-final">
+<path id="Rectangle 1" d="M12.7221 2.06009C18.8008 1.28574 24.0855 1.33464 29.8572 2.08119C36.4622 2.93553 41.1667 8.9512 41.1667 15.8308C41.1667 23.9324 34.5991 30.5 26.4975 30.5H16.2484C8.10309 30.5 1.5 23.8969 1.5 15.7516C1.5 8.89212 6.15342 2.89686 12.7221 2.06009Z" fill="#00AEEF" stroke="#2D2D2D" stroke-width="3"/>
+<path class="bgChatbot" id="Rectangle 3" d="M12.6589 1.5641C18.7813 0.784177 24.1089 0.833495 29.9213 1.58532C36.811 2.47649 41.6667 8.73731 41.6667 15.8308C41.6667 24.2085 34.8752 31 26.4975 31H16.2484C7.82695 31 1 24.1731 1 15.7516C1 8.68104 5.80224 2.43756 12.6589 1.5641Z" fill="#00AEEF" stroke="#2D2D2D" stroke-width="2"/>
+<path  id="Line 7" d="M5.85516 5.37109C5.57902 5.37109 5.35516 5.59495 5.35516 5.87109C5.35516 6.14724 5.57902 6.37109 5.85516 6.37109V5.37109ZM5.85516 6.37109H16.5347V5.37109H5.85516V6.37109Z" fill="#2D2D2D"/>
+<g id="Face-contorn">
+<mask id="path-4-inside-1_101_105" fill="white">
+<path d="M6.39996 17.4157C6.39996 12.799 10.143 9.0564 14.7598 9.0564C19.6337 9.0564 23.8174 9.0564 28.5336 9.0564C33.1962 9.0564 36.9777 12.8362 36.9777 17.4989V17.4989C36.9777 23.3956 31.0863 27.4758 25.5663 25.4021L24.4516 24.9834C22.4444 24.2293 20.2288 24.2466 18.2336 25.0318L17.8206 25.1944C12.336 27.3528 6.39996 23.3098 6.39996 17.4157V17.4157Z"/>
+</mask>
+<path d="M6.39996 17.4157C6.39996 12.799 10.143 9.0564 14.7598 9.0564C19.6337 9.0564 23.8174 9.0564 28.5336 9.0564C33.1962 9.0564 36.9777 12.8362 36.9777 17.4989V17.4989C36.9777 23.3956 31.0863 27.4758 25.5663 25.4021L24.4516 24.9834C22.4444 24.2293 20.2288 24.2466 18.2336 25.0318L17.8206 25.1944C12.336 27.3528 6.39996 23.3098 6.39996 17.4157V17.4157Z" fill="white"/>
+<path d="M24.4516 24.9834L24.0999 25.9195L24.4516 24.9834ZM18.2336 25.0318L17.8674 24.1013L18.2336 25.0318ZM25.5663 25.4021L25.2146 26.3383L25.5663 25.4021ZM14.7598 10.0564C19.6337 10.0564 23.8174 10.0564 28.5336 10.0564L28.5336 8.0564C23.8174 8.0564 19.6337 8.0564 14.7598 8.0564L14.7598 10.0564ZM25.918 24.466L24.8033 24.0473L24.0999 25.9195L25.2146 26.3383L25.918 24.466ZM17.8674 24.1013L17.4544 24.2638L18.1868 26.1249L18.5998 25.9624L17.8674 24.1013ZM24.8033 24.0473C22.5643 23.2062 20.093 23.2254 17.8674 24.1013L18.5998 25.9624C20.3647 25.2678 22.3245 25.2525 24.0999 25.9195L24.8033 24.0473ZM5.39996 17.4157C5.39996 24.0149 12.0461 28.5416 18.1868 26.1249L17.4544 24.2638C12.6259 26.1641 7.39996 22.6047 7.39996 17.4157H5.39996ZM35.9777 17.4989C35.9777 22.6971 30.7842 26.294 25.918 24.466L25.2146 26.3383C31.3885 28.6575 37.9777 24.094 37.9777 17.4989H35.9777ZM28.5336 10.0564C32.6442 10.0564 35.9777 13.3888 35.9777 17.4989H37.9777C37.9777 12.2837 33.7483 8.0564 28.5336 8.0564L28.5336 10.0564ZM14.7598 8.0564C9.59083 8.0564 5.39996 12.2466 5.39996 17.4157H7.39996C7.39996 13.3514 10.6953 10.0564 14.7598 10.0564L14.7598 8.0564Z" fill="#2D2D2D" mask="url(#path-4-inside-1_101_105)"/>
+</g>
+<path id="Line 10" d="M36.3121 19.0638L40.8194 19.0118" stroke="#2D2D2D" stroke-linecap="round"/>
+<line id="Line 12" x1="23.195" y1="5.94678" x2="23.195" y2="9.48578" stroke="#2D2D2D" stroke-linecap="round"/>
+<path id="Line 22" d="M26.4 27.5V25.5" stroke="#2D2D2D" stroke-linecap="round"/>
+<circle class="bgChatbot" id="Ellipse 1" cx="16.9556" cy="5.95556" r="1.45556" fill="#00AEEF" stroke="#2D2D2D"/>
+<line id="Line 11" x1="23.2554" y1="5.90002" x2="32.9221" y2="5.90002" stroke="#2D2D2D" stroke-linecap="round"/>
+<path id="Line 13" d="M33 5.80005L34.9101 4.23723" stroke="#2D2D2D" stroke-linecap="round"/>
+<line id="Line 14" x1="1.21112" y1="16.2112" x2="6.90393" y2="16.2112" stroke="#2D2D2D" stroke-linecap="round"/>
+<line id="Line 21" x1="7.49976" y1="27.5709" x2="26.3716" y2="27.5709" stroke="#2D2D2D" stroke-linecap="round"/>
+<rect class="Eye-left-s" x="15" y="13" width="3.55555" height="7.82222" rx="1.77778" fill="#2D2D2D"/>
+<rect class="Eye-right-s" x="24.9556" y="13" width="3.55555" height="7.82222" rx="1.77778" fill="#2D2D2D"/>
+</g>
+</svg>`;
 //////////////////////////////
 // HTML TEMPLATES
 //////////////////////////////
@@ -10,7 +35,7 @@
 const htmlChatbotText = (text) => {
   return `
   <div class="chatbot-msg">
-    <img class="logo--chat" src="./img/Logo-header.svg" alt="" />
+    ${chatbotLogoFace}
 
     <div class="chatbot-msg-content">
       <p class="chatbot-text">
@@ -29,7 +54,7 @@ const htmlChatbotText = (text) => {
 const htmlChatbotTextNoface = (text) => {
   return `
   <div class="chatbot-msg chatbot-msg-no-face">
-    <img class="logo--chat" src="./img/Logo-header.svg" alt="" />
+    ${chatbotLogoFace}
 
     <div class="chatbot-msg-content">
       <p class="chatbot-text">
@@ -61,7 +86,7 @@ const htmlChatbotOptions = (text, is_final, prevQuery, ...options) => {
 
   return `
           <div class="chatbot-msg">
-            <img class="logo--chat" src="./img/Logo-header.svg" alt="" />
+            ${chatbotLogoFace}
             <div class="chatbot-msg-content">
               <div class="chatbot-options">
                 <p class="chatbot-options-text">
@@ -82,7 +107,7 @@ const htmlChatbotOptions = (text, is_final, prevQuery, ...options) => {
 const htmlUserInput = (text) => {
   return `
   <div class="chatbot-msg chatbot-msg-user">
-    <img class="logo--chat" src="./img/Logo-header.svg" alt="" />
+    ${chatbotLogoFace}
     <div class="chatbot-msg-content">
       <p class="chatbot-text">${text}</p>
     </div>
@@ -96,7 +121,7 @@ const htmlUserInput = (text) => {
 const htmlChatbotFormReview = () => {
   return `
   <div class="chatbot-msg">
-    <img class="logo--chat" src="./img/Logo-header.svg" alt="" />
+    ${chatbotLogoFace}
 
     <div class="container-stars chatbot-msg-content">
       <form class="form-stars" action="#">
@@ -162,7 +187,7 @@ const htmlChatbotFormReview = () => {
 const htmlChatbotFormContact = () => {
   return `
   <div class="chatbot-msg">
-    <img class="logo--chat" src="./img/Logo-header.svg" alt="" />
+    ${chatbotLogoFace}
 
     <div class="chatbot-msg-content">
       <form class="form-to-mail" action="#">
@@ -197,7 +222,7 @@ const htmlChatbotFormContact = () => {
 const htmlChatbotLoading = () => {
   return `
   <div class="chatbot-msg chatbot-loader">
-    <img class="logo--chat" src="./img/Logo-header.svg" alt="" />
+    ${chatbotLogoFace}
 
     <div class="chatbot-msg-content loader">
       <span></span>
@@ -211,6 +236,7 @@ const htmlChatbotLoading = () => {
 //////////////////////////////
 // HTML INSERT TEMPLATES
 //////////////////////////////
+
 const chatbotBox = document.querySelector(".chatbot-box");
 const chatbotChat = document.querySelector(".chatbot-chat");
 
@@ -219,7 +245,9 @@ const chatbotChat = document.querySelector(".chatbot-chat");
  * @param  {string} text      Texto a mostrar
  */
 const insertHtmlChatbotText = function (text) {
+  blockChatboFace();
   chatbotChat.insertAdjacentHTML("beforeend", htmlChatbotText(text));
+  updateChatbotFace();
   updateScrollBar();
 };
 
@@ -228,7 +256,9 @@ const insertHtmlChatbotText = function (text) {
  * @param  {string} text      Texto a mostrar
  */
 const insertHtmlChatbotTextNoFace = function (text) {
+  blockChatboFace();
   chatbotChat.insertAdjacentHTML("beforeend", htmlChatbotTextNoface(text));
+  updateChatbotFace();
   updateScrollBar();
 };
 
@@ -236,7 +266,9 @@ const insertHtmlChatbotTextNoFace = function (text) {
  * Insertar el cuadro de animacion
  */
 const insertHtmlChatbotLoading = function () {
+  blockChatboFace();
   chatbotChat.insertAdjacentHTML("beforeend", htmlChatbotLoading());
+  updateChatbotFace();
   updateScrollBar();
 };
 
@@ -253,6 +285,7 @@ const insertHtmlChatbotOptions = function (
   prevQuery,
   ...options
 ) {
+  blockChatboFace();
   // Cuando ya hay mas de un chatbotOptions, se bloquea el ultimo chatbotOptions
   if (optionsBox !== undefined) {
     blockLastChatbotOptions();
@@ -263,6 +296,7 @@ const insertHtmlChatbotOptions = function (
     "beforeend",
     htmlChatbotOptions(text, isFinal, prevQuery, ...options)
   );
+  updateChatbotFace();
 
   // Actualizamos el ultimo optionsBox
   optionsBox = [...document.querySelectorAll(".chatbot-options")].at(-1);
@@ -278,7 +312,9 @@ const insertHtmlChatbotOptions = function (
  * @param  {string} text      Texto a mostrar
  */
 const insertHtmlUserInput = function (text) {
+  blockChatboFace();
   chatbotChat.insertAdjacentHTML("beforeend", htmlUserInput(text));
+  updateChatbotFace();
   updateScrollBar();
 };
 
@@ -286,7 +322,9 @@ const insertHtmlUserInput = function (text) {
  * Insertar el chatbot review
  */
 const insertHtmlChatbotFormReview = function () {
+  blockChatboFace();
   chatbotChat.insertAdjacentHTML("beforeend", htmlChatbotFormReview());
+  updateChatbotFace();
   updateScrollBar();
 
   // Habilitamos al ultimo formulario
@@ -297,8 +335,10 @@ const insertHtmlChatbotFormReview = function () {
 /*
  * Insertar formulario de contacto
  */
-const insertHtmlFormEmail = function () {
+const insertHtmlFormContact = function () {
+  blockChatboFace();
   chatbotChat.insertAdjacentHTML("beforeend", htmlChatbotFormContact());
+  updateChatbotFace();
   updateScrollBar();
 
   // Habilitamos al ultimo formulario
@@ -362,10 +402,12 @@ const prepareHtmlOptionsDB = async function (
   // Esta serie de if's es para cuando se llega a una respuesta final y se muestran las ultimas opciones
   if (query == "Si") {
     console.log("ENTEEEER");
+    blockChatboFace();
     blockLastChatbotOptions();
     insertHtmlChatbotTextNoFace("Has seleccionado que sí");
     insertHtmlChatbotText("Nos ayudarías mucho calificando nuestro servicio: ");
     insertHtmlChatbotFormReview();
+    updateChatbotFace();
     return;
   }
 
@@ -377,8 +419,10 @@ const prepareHtmlOptionsDB = async function (
 
   if (query == "Solicitar información adicional") {
     console.log("Reiniciar chatbot");
+    blockChatboFace();
     blockLastChatbotOptions();
-    insertHtmlFormEmail();
+    insertHtmlFormContact();
+    updateChatbotFace();
     return;
   }
 
@@ -402,7 +446,9 @@ const prepareHtmlOptionsDB = async function (
   // Atrapando los errores que pudieron generarse al hacer el fetch
   if (dataQnParent == MSGEROR || dataOptions == MSGEROR) {
     removeLoader();
+    blockChatboFace();
     insertHtmlChatbotText("Ha ocurrido un error");
+    updateChatbotFace();
     return;
   }
 
@@ -422,10 +468,13 @@ const prepareHtmlOptionsDB = async function (
   // En caso de que se haya seleccionala opcion de "Contacto" se muestra el formulario
   if (dataQnParent[0].texto == "mostrarFormulario()") {
     console.log("SIUUUUUUUUU");
-    insertHtmlFormEmail();
+    blockChatboFace();
+    insertHtmlFormContact();
+    updateChatbotFace();
     return;
   }
 
+  blockChatboFace();
   // Finalmente se insertan las opciones obtenidas de la base de datos
   insertHtmlChatbotOptions(
     textOption,
@@ -433,11 +482,14 @@ const prepareHtmlOptionsDB = async function (
     dataQnParent[0].nombre,
     ...htmlOptions
   );
+  updateChatbotFace();
 
   // Cuando se llegar a una pregunta final, se muestran las siguientes opciones a manera de cierre
   if (dataOptions.length == 0) {
     showLoader(TIMELOADER).then(() => {
       removeLoader();
+      blockChatboFace();
+
       insertHtmlChatbotOptions(
         "¿Pudimos ayudarte a encontrar lo que buscabas?",
         true,
@@ -448,6 +500,8 @@ const prepareHtmlOptionsDB = async function (
           "Solicitar información adicional 🎈",
         ]
       );
+      updateChatbotFace();
+
       return;
     });
   }
@@ -478,6 +532,8 @@ const handleShowChatbot = (function () {
     // Solo entra a este if cuando se abre por primera vez el chatbot
     if (!flagChatbotOpen) {
       insertHtmlChatbotText(msgWelcome);
+      blockChatboFace();
+      updateChatbotFace();
       flagChatbotOpen = true;
       prepareHtmlOptionsDB();
     }
@@ -569,11 +625,15 @@ const activeFormReview = function () {
       postScoreDB(answer)
         .then((response) => response.json())
         .then((data) => {
+          blockChatboFace();
           insertHtmlChatbotText(data);
+          updateChatbotFace();
           removeLoader();
         })
         .catch(() => {
+          blockChatboFace();
           insertHtmlChatbotText("Ocurrio un error");
+          updateChatbotFace();
           removeLoader();
         });
     });
@@ -663,14 +723,18 @@ const activeFormContact = function () {
         postPersonDB(newPerson)
           .then((response) => response.json())
           .then((data) => {
+            blockChatboFace();
             insertHtmlChatbotText(
               `Muchas gracias, ${data.nombre}. Hemos recibido tus datos`
             );
+            updateChatbotFace();
             removeLoader();
           })
           .catch((err) => {
+            blockChatboFace();
             console.log("Ocurrio un error");
             insertHtmlChatbotText("Ocurrio un error");
+            updateChatbotFace();
             removeLoader();
           });
       });
@@ -691,7 +755,9 @@ const activateSearchBar = (function () {
   searchBarEl.addEventListener("keyup", function (e) {
     e.preventDefault();
     if (e.key === "Enter" || e.keyCode === 13) {
+      blockChatboFace();
       insertHtmlUserInput(searchBarEl.value);
+      updateChatbotFace();
       searchBarEl.value = "";
     }
   });
@@ -799,7 +865,6 @@ btnChatbotOptions.addEventListener("click", function () {
   showLoader(TIMELOADER).then(() => {
     removeLoader();
 
-    // insertHtmlChatbotOptions(someText, 1, 2, 3, 4);
     prepareHtmlOptionsDB();
   });
 });
@@ -813,5 +878,42 @@ btnChatbotReview.addEventListener("click", function () {
 });
 
 btnChatbotForm.addEventListener("click", function () {
-  insertHtmlFormEmail();
+  insertHtmlFormContact();
 });
+
+//
+//
+//
+//
+// Para la cara feliz
+let lastChatbotFace = [...document.querySelectorAll(".logo--chat")].at(-1);
+let lastEyeLeft = [...document.querySelectorAll(".Eye-left-s")].at(-1);
+let lastEyeRight = [...document.querySelectorAll(".Eye-right-s")].at(-1);
+
+const blockChatboFace = function () {
+  if (lastChatbotFace != undefined) {
+    lastChatbotFace.classList.add("block-chatbot-logo--chat");
+  }
+};
+
+const updateChatbotFace = function () {
+  lastChatbotFace = [...document.querySelectorAll(".logo--chat")].at(-1);
+
+  lastEyeLeft = [...document.querySelectorAll(".Eye-left-s")].at(-1);
+  lastEyeRight = [...document.querySelectorAll(".Eye-right-s")].at(-1);
+};
+
+const changeEyes = function () {
+  updateChatbotFace();
+  lastEyeLeft.remove();
+  lastChatbotFace.insertAdjacentHTML(
+    "beforeEnd",
+    '<path class="Eye-left" d="M12 15.6194C15.3948 14.8909 16.949 15.2606 18.3386 19" stroke="#2D2D2D" stroke-width="1.5" stroke-linecap="round"/>'
+  );
+
+  lastEyeRight.remove();
+  lastChatbotFace.insertAdjacentHTML(
+    "beforeEnd",
+    '<path class="Eye-Right" d="M29.5949 15.6194C26.2001 14.8909 24.6459 15.2606 23.2563 19" stroke="#2D2D2D" stroke-width="1.5" stroke-linecap="round"/>'
+  );
+};
